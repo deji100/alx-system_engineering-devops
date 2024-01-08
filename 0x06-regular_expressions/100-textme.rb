d1a0c76e73a3d4ac94aa895477b5d888ced27554
deji100 <adeyemideji9@gmail.com>
2024-01-07 20:55:49 -0800

    updated 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[SENDER:(.*?)\] \[RECEIVER:(.*?)\] \[FLAGS:(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(^\+\d{1|2|3}[0-9]*?)\] \[flags:(.*?)\]/).join(",")
